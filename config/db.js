9eb4a941182d0c3a6628397745ff7e1f28f6369b
Kpsingh0-7 <kpsingh954321@gmail.com>
2025-05-28 12:22:59 +0530

    6

--- config/db.js
+++ config/db.js
@@ -16,10 +16,13 @@
 //   database: "railway",
 // });
 
+import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({ 
+  
  host: '195.201.175.72', // replace with your DB host 
  user: 'kpremote', // replace with your DB user 
+ port: '3306',
  password: 'z*4QO2HFfEsR4YM%', // replace with your DB password 
  database: 'gupshup' // replace with your DB name 
 });
