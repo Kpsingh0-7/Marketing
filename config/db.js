8c8c1aa824a3906523f27c997d1b13b21654bbb1
Kpsingh0-7 <kpsingh954321@gmail.com>
2025-06-13 12:26:16 +0530

    13-06-25

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
-import mysql from 'mysql2/promise';
-export const pool = mysql.createPool({
- host: 'localhost', // replace with your DB host
- user: 'root', // replace with your DB user
- password: 'Kpsingh@1234', // replace with your DB password
- database: 'Marketing' // replace with your DB name
-});
+// import mysql from 'mysql2/promise';
+// export const pool = mysql.createPool({
+//  host: 'localhost', // replace with your DB host
+//  user: 'root', // replace with your DB user
+//  password: 'Kpsingh@1234', // replace with your DB password
+//  database: 'Marketing' // replace with your DB name
+// });
 
 // import mysql from "mysql2/promise";
 
@@ -16,13 +16,13 @@ export const pool = mysql.createPool({
 //   database: "railway",
 // });
 
-// import mysql from "mysql2/promise";
+import mysql from "mysql2/promise";
 
-// export const pool = mysql.createPool({ 
+export const pool = mysql.createPool({ 
   
-//  host: '195.201.175.72', // replace with your DB host 
-//  user: 'kpremote', // replace with your DB user 
-//  port: '3306',
-//  password: 'z*4QO2HFfEsR4YM%', // replace with your DB password 
-//  database: 'gupshup' // replace with your DB name 
-// });
+ host: '195.201.175.72', // replace with your DB host 
+ user: 'kpremote', // replace with your DB user 
+ port: '3306',
+ password: 'z*4QO2HFfEsR4YM%', // replace with your DB password 
+ database: 'gupshup' // replace with your DB name 
+});
