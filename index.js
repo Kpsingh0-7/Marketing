d61c9a00f33a350ec289ec42b89563597a9ba9c8
Kpsingh0-7 <kpsingh954321@gmail.com>
2025-06-14 10:11:49 +0530

    14-06-25

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // === CORS config ===
 const corsOptions = {
-  origin: 'https://kpw-ahds.onrender.com', // Adjust in production
+  origin: 'https://kpw-iu7e.onrender.com', // Adjust in production
   methods: ['GET', 'POST'],
   credentials: true,
 };
